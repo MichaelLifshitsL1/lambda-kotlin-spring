a39014b36faf1d2c0a817fdc33971569103fe092
MichaelLifshitsL1 <mlifshits@loyalty.com>
2019-05-29 11:19:36 -0400

    working version

---
+++ src/main/kotlin/com/demo/Application.kt
@@ -0,0 +1,29 @@
+package com.demo
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.messaging.Message
+import org.springframework.messaging.support.GenericMessage
+import java.util.function.Function
+
+@SpringBootApplication
+class Application {
+
+    @Bean
+    fun function(objectMapper: ObjectMapper): Function<Message<Any>, Message<String>> {
+        return Function {
+            GenericMessage("Saved to S3",
+                    mapOf("Content-type" to "text/plain",
+                            "statusCode" to 202))
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, *args)
+        }
+    }
+}
